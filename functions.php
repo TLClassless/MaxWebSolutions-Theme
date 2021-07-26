3478b02a4b9c60d00cb90bea79f0b3aa721edde5
Ben <classle55sc2@gmail.com>
2021-07-26 18:39:34 +0100

    remove jQuery

--- functions.php
+++ functions.php
@@ -12,16 +12,6 @@ function load_stylesheets()
 
 add_action('wp_enqueue_scripts', 'load_stylesheets');
 
-// include jQuery
-function include_jquery() 
-    {
-        wp_deregister_script('jquery');
-        wp_enqueue_script('jquery', get_template_directory_uri() . '/js/jQuery/jquery-3.6.0.min.js', '', 1, true);
-        add_action('wp_enqueue_scripts', 'jquery');
-    }
-
-add_action('wp_enqueue_scripts', 'include_jquery');
-
 
 // enqueue scripts
 function load_js() 
